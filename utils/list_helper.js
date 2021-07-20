4bf625e99a47400b0cf3db74d82e834a874d713a
Kyle Oda <1624udon@gmail.com>
2021-07-20 02:39:09 -0700

    finished exercise 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -4,7 +4,14 @@ const dummy = (blogs) => {
 
 const totalLikes = blogs => blogs.reduce((total, blog) => blog.likes + total, 0)
 
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((prev, current) => {
+    return prev.likes <= current.likes ? current : prev
+  })
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
