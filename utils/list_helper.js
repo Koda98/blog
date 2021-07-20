41072597e18e3a34fe01a28b114ee0a1684ab6ab
Kyle Oda <1624udon@gmail.com>
2021-07-20 15:38:55 -0700

    finished exercise 4.6

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,17 +1,37 @@
+const lodash = require('lodash')
+
 const dummy = (blogs) => {
   return 1
 }
 
 const totalLikes = blogs => blogs.reduce((total, blog) => blog.likes + total, 0)
 
-const favoriteBlog = (blogs) => {
-  return blogs.reduce((prev, current) => {
-    return prev.likes <= current.likes ? current : prev
-  })
+const favoriteBlog = blogs => {
+  const mostLikedBlog = blogs.reduce((prev, current) => prev.likes <= current.likes ? current : prev)
+  const favBlog = {
+    title: mostLikedBlog.title,
+    author: mostLikedBlog.author,
+    likes: mostLikedBlog.likes
+  }
+  return favBlog
+}
+
+const mostBlogs = blogs => {
+  const maxBlogCount = lodash.chain(blogs)
+    .countBy('author')
+    .toPairs()
+    .values()
+    .max()
+    .value() 
+  return {
+    author: maxBlogCount[0],
+    blogs: maxBlogCount[1]
+  }
 }
 
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
