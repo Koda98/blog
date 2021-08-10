e3c8f1c461b8b95d97fd9d9ab0ded2d00d5e0484
Kyle Oda <1624udon@gmail.com>
2021-08-09 18:42:47 -0700

    finished exercise 4.13

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,4 +1,5 @@
 const blogsRouter = require('express').Router()
+const { notesInDb } = require('../../../examples/notes_example/backend/tests/test_helper')
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', (request, response) => {
@@ -20,25 +21,12 @@ blogsRouter.post('/', async (request, response) => {
   })
 
   const savedBlog = await blog.save()
-  response.json(savedBlog)
+  response.status(201).json(savedBlog)
 })
 
-
-blogsRouter.post('/', (request, response) => {
-  const body = request.body
-
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes || 0
-  })
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
 })
 
 module.exports = blogsRouter
--- tests/bloglist_api.test.js
+++ tests/bloglist_api.test.js
@@ -59,7 +59,6 @@ test('likes property defaults to 0 if missing', async () => {
     .send(newBlog)
     .expect(201)
     .expect('Content-Type', /application\/json/)
-
     
   const expectedBlog = {
     title: 'Circle Syl',
@@ -90,6 +89,27 @@ test('blog without title and url is not added', async () => {
   const blogsAtEnd = await helper.blogsInDb()
 
   expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+})
+
+describe('deletion of a blog', () => {
+  test('succeeds with status code 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
+
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204)
+    
+      const blogsAtEnd = await helper.blogsInDb()
+
+      expect(blogsAtEnd).toHaveLength(
+        helper.initialBlogs.length - 1
+      )
+
+      const titles = blogsAtEnd.map(r => r.title)
+
+      expect(titles).not.toContain(blogToDelete.title)
+  })
 })
 
 afterAll(() => {
